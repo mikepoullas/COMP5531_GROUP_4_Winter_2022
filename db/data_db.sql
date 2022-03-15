8a54429bbe1ba140946a202d6ba0df1601ba86e4
shafiq.imtiaz <shafiqimtiaz@gmail.com>
2022-03-15 02:26:51 -0400

    update DB

--- db/data_db.sql
+++ db/data_db.sql
@@ -3,11 +3,11 @@ INSERT INTO roles (role_name, role_description) VALUES("Student", "Course Studen
 INSERT INTO roles (role_name, role_description) VALUES("TA", "Course Assistant");
 INSERT INTO roles (role_name, role_description) VALUES("Professor", "Course Instructor");
 
-INSERT INTO users VALUES(10000, "Admin", "CGA","1980-01-01", "admin", "admin","admin@cga.com", CURRENT_TIMESTAMP, 1, 1);
-INSERT INTO users (first_name, last_name, dob, username, password, email, created_on, is_first_login, role_id) VALUES("John", "Doe","1990-01-01", "john123", "password","john@doe.com", CURRENT_TIMESTAMP,1, 2);
-INSERT INTO users (first_name, last_name, dob, username, password, email, created_on, is_first_login, role_id) VALUES("Yogesh", "Homeboi","1980-01-01", "yogesh123", "password","yogesh@homeboi.com", CURRENT_TIMESTAMP, 1, 3);
-INSERT INTO users (first_name, last_name, dob, username, password, email, created_on, is_first_login, role_id) VALUES("Lord", "Desai","1900-01-01", "desai123", "password","lord@desai.com", CURRENT_TIMESTAMP, 1, 4);
-INSERT INTO users (first_name, last_name, dob, username, password, email, created_on, is_first_login, role_id) VALUES("Mike", "Poullas","1966-03-04", "mikep", "password","mikepoullas@yahoo.com", CURRENT_TIMESTAMP, 1, 2);
+INSERT INTO users VALUES(10000, "Admin", "CGA","1980-01-01", "admin@cga.com", "admin", "admin", CURRENT_TIMESTAMP, 1, 1);
+INSERT INTO users (first_name, last_name, dob, email, username, password, created_on, is_first_login, role_id) VALUES("John", "Doe","1990-01-01", "john@doe.com", "john123", "password", CURRENT_TIMESTAMP,1, 2);
+INSERT INTO users (first_name, last_name, dob, email, username, password, created_on, is_first_login, role_id) VALUES("Yogesh", "Homeboi","1980-01-01", "yogesh@homeboi.com", "yogesh123", "password", CURRENT_TIMESTAMP, 1, 3);
+INSERT INTO users (first_name, last_name, dob, email, username, password, created_on, is_first_login, role_id) VALUES("Lord", "Desai","1900-01-01", "lord@desai.com", "desai123", "password", CURRENT_TIMESTAMP, 1, 4);
+INSERT INTO users (first_name, last_name, dob, email, username, password, created_on, is_first_login, role_id) VALUES("Mike", "Poullas","1966-03-04", "mikepoullas@yahoo.com", "mikep", "password", CURRENT_TIMESTAMP, 1, 2);
 
 INSERT INTO course VALUES(50000, "Files and DB", 5531);
 INSERT INTO course (course_name, course_number) VALUES("Tools and Techniques", 5541);
