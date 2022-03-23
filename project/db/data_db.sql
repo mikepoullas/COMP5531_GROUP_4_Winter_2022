13fff7886b1b050e2415497db3f5822c3d9552e9
shafiq.imtiaz <shafiqimtiaz@gmail.com>
2022-03-23 18:32:23 -0400

    update DB

--- project/db/data_db.sql
+++ project/db/data_db.sql
@@ -4,9 +4,10 @@ INSERT INTO roles (role_name, role_description) VALUES("TA", "Course Assistant")
 INSERT INTO roles (role_name, role_description) VALUES("Student", "Course Student");
 
 INSERT INTO users VALUES(10000, "Admin", "CGA","1980-01-01", "admin@cga.com", "admin", "admin", CURRENT_TIMESTAMP, 1, 1);
-INSERT INTO users (first_name, last_name, dob, email, username, password, created_on, first_login, role_id) VALUES("Lord", "Desai","1900-01-01", "lord@desai.com", "desai123", "password", CURRENT_TIMESTAMP, 1, 2);
-INSERT INTO users (first_name, last_name, dob, email, username, password, created_on, first_login, role_id) VALUES("Yogesh", "Homeboi","1980-01-01", "yogesh@homeboi.com", "yogesh123", "password", CURRENT_TIMESTAMP, 1, 3);
+INSERT INTO users (first_name, last_name, dob, email, username, password, created_on, first_login, role_id) VALUES("Bipin", "Desai","1900-01-01", "lord@desai.com", "desai123", "password", CURRENT_TIMESTAMP, 1, 2);
+INSERT INTO users (first_name, last_name, dob, email, username, password, created_on, first_login, role_id) VALUES("Yogesh", "Yadav","1980-01-01", "yogesh@homeboi.com", "yogesh123", "password", CURRENT_TIMESTAMP, 1, 3);
 INSERT INTO users (first_name, last_name, dob, email, username, password, created_on, first_login, role_id) VALUES("John", "Doe","1990-01-01", "john@doe.com", "john123", "password", CURRENT_TIMESTAMP,1, 4);
+INSERT INTO users (first_name, last_name, dob, email, username, password, created_on, first_login, role_id) VALUES("Mary", "Jane","1990-04-12", "mary@jane.com", "mary123", "password", CURRENT_TIMESTAMP,1, 4);
 
 INSERT INTO course VALUES(50000, "Files and DB", 5531);
 INSERT INTO course (course_name, course_number) VALUES("Tools and Techniques", 5541);
@@ -14,7 +15,8 @@ INSERT INTO course (course_name, course_number) VALUES("Operating Systems", 5461
 INSERT INTO course (course_name, course_number) VALUES("Algos and Data Structures", 5511);
 
 -- Student Table
-INSERT INTO student VALUES(20000, 10003);
+INSERT INTO student (student_id, user_id) VALUES(20000, 10003);
+INSERT INTO student (user_id) VALUES(10004);
 
 -- TA Table
 INSERT INTO ta (ta_id, user_id) VALUES(30000, 10002);
@@ -41,10 +43,15 @@ INSERT INTO student_groups (group_name, group_leader_sid) VALUES("Group_2", 0);
 
 -- Assign Student to Group  
 INSERT INTO member_of_group (student_id, group_id) VALUES(20000, 70000);
+INSERT INTO member_of_group (student_id, group_id) VALUES(20001, 70001);
 
 -- Course Section Table
 INSERT INTO course_section (section_id, section_name, course_id) VALUES(60000, "AA", 50000);
 INSERT INTO course_section (section_name, course_id) VALUES("BB", 50001);
+
+-- Assign Groups to Section
+INSERT INTO section_groups (section_id, group_id) VALUES(60000, 70000);
+INSERT INTO section_groups (section_id, group_id) VALUES(60000, 70002);
 
 -- Course Anncoucement Table
 INSERT INTO announcement (announcement_id, title, posted_by_uid, posted_on, content, section_id) VALUES(2200000, "Project Due!!", 10003, "2022-03-11", "Must submit project on time!", 60000);
