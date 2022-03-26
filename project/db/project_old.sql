053e1b734cc1af8160eeb979c54203c5871655fe
shafiq.imtiaz <shafiqimtiaz@gmail.com>
2022-03-26 19:55:57 -0400

    changes in ERD and DB

--- project/db/project_old.sql
+++ project/db/project_old.sql
@@ -1,141 +1,192 @@
+create database cga;
+use cga;
+
+/*
+
+10000 - user
+20000 - student
+30000 - ta
+40000 - professor
+50000 - course
+60000 - section
+70000 - groups
+
+1100000 - announcement
+2200000 - discussion
+3300000 - comment
+4400000 - files
+
+*/
+
 CREATE TABLE Roles
 (
-  role_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  role_name VARCHAR(15) NOT NULL,
-  role_description VARCHAR(100) NOT NULL,
-  PRIMARY KEY (role_id)
+  role_id INT NOT NULL AUTO_INCREMENT,
+  role_name VARCHAR(30) NOT NULL,
+  role_description VARCHAR(255) NOT NULL,
+  PRIMARY KEY (role_id),
+  UNIQUE (role_name)
 );
 
-CREATE TABLE User
+CREATE TABLE Users
 (
-  user_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  role_id SMALLINT UNSIGNED NOT NULL,
-  username VARCHAR(30) NOT NULL,
-  password VARCHAR(25) NOT NULL,
-  first_name VARCHAR(25) NOT NULL,
+  user_id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   dob DATE NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  is_first_login TINYINT NOT NULL,
+  email VARCHAR(30) NOT NULL,
+  username VARCHAR(30) NOT NULL,
+  password VARCHAR(30) NOT NULL,
+  created_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+  first_login TINYINT NOT NULL,
+  role_id INT NOT NULL,
   PRIMARY KEY (user_id),
   FOREIGN KEY (role_id) REFERENCES Roles(role_id),
+  UNIQUE (email),
   UNIQUE (username)
-);
-
-CREATE TABLE Course
-(
-  course_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  course_name VARCHAR(25) NOT NULL,
-  course_number MEDIUMINT UNSIGNED NOT NULL,
-  max_groups SMALLINT UNSIGNED,
-  max_members SMALLINT UNSIGNED,
-  PRIMARY KEY (course_id)
 );
 
 CREATE TABLE Student
 (
-  student_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  user_id MEDIUMINT UNSIGNED NOT NULL,
-  course_id MEDIUMINT UNSIGNED NOT NULL,
+  student_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
   PRIMARY KEY (student_id),
-  FOREIGN KEY (user_id) REFERENCES User(user_id),
-  FOREIGN KEY (course_id) REFERENCES Course(course_id)
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
 CREATE TABLE Professor
 (
-  professor_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  user_id MEDIUMINT UNSIGNED NOT NULL,
-  course_id MEDIUMINT UNSIGNED NOT NULL,
+  professor_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
   PRIMARY KEY (professor_id),
-  FOREIGN KEY (user_id) REFERENCES User(user_id),
-  FOREIGN KEY (course_id) REFERENCES Course(course_id)
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
 
 CREATE TABLE TA
 (
-  ta_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  user_id MEDIUMINT UNSIGNED NOT NULL,
-  course_id MEDIUMINT UNSIGNED NOT NULL,
+  ta_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT NOT NULL,
   PRIMARY KEY (ta_id),
-  FOREIGN KEY (user_id) REFERENCES User(user_id),
-  FOREIGN KEY (course_id) REFERENCES Course(course_id)
+  FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE Course
+(
+  course_id INT NOT NULL AUTO_INCREMENT,
+  course_name VARCHAR(30) NOT NULL,
+  course_number INT NOT NULL UNIQUE,
+  PRIMARY KEY (course_id),
+  UNIQUE (course_name),
+  UNIQUE (course_number)
 );
 
 CREATE TABLE Course_Section
 (
-  section_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  course_id MEDIUMINT UNSIGNED NOT NULL,
-  section_name VARCHAR(25) NOT NULL,
+  section_id INT NOT NULL AUTO_INCREMENT,
+  section_name VARCHAR(30) NOT NULL,
+  course_id INT NOT NULL,
   PRIMARY KEY (section_id),
-  FOREIGN KEY (course_id) REFERENCES Course(course_id)
+  FOREIGN KEY (course_id) REFERENCES Course(course_id),
+  UNIQUE (section_name)
 );
 
-CREATE TABLE Student_Groups
+CREATE TABLE Student_groups
 (
-  group_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  group_name VARCHAR(50) NOT NULL,
-  group_leader_student_id MEDIUMINT UNSIGNED,
-  project VARCHAR(256),
-  assignment VARCHAR(256),
-  PRIMARY KEY (group_id)
+  group_id INT NOT NULL AUTO_INCREMENT,
+  group_name VARCHAR(30) NOT NULL,
+  group_leader_sid INT,
+  PRIMARY KEY (group_id),
+  UNIQUE (group_name)
 );
 
-CREATE TABLE Is_a_member_of
+CREATE TABLE member_of_group
 (
-  student_id MEDIUMINT UNSIGNED NOT NULL,
-  group_id MEDIUMINT UNSIGNED NOT NULL,
-  PRIMARY KEY (student_id, group_id),
+  student_id INT NOT NULL,
+  group_id INT NOT NULL,
   FOREIGN KEY (student_id) REFERENCES Student(student_id),
-  FOREIGN KEY (group_id) REFERENCES Student_Groups(group_id)
+  FOREIGN KEY (group_id) REFERENCES Student_groups(group_id),
+  UNIQUE (group_id)
+);
+
+CREATE TABLE section_groups
+(
+  section_id INT NOT NULL,
+  group_id INT NOT NULL,
+  FOREIGN KEY (section_id) REFERENCES Course_Section(section_id),
+  FOREIGN KEY (group_id) REFERENCES Student_groups(group_id),
+  UNIQUE (group_id)
+);
+
+CREATE TABLE User_Course
+(
+  user_id INT NOT NULL,
+  course_id INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES Users(user_id),
+  FOREIGN KEY (course_id) REFERENCES Course(course_id),
+  UNIQUE (user_id, course_id)
+);
+
+CREATE TABLE Group_assignment
+(
+  group_asn_id INT NOT NULL AUTO_INCREMENT,
+  details VARCHAR(255) NOT NULL,
+  group_id INT NOT NULL,
+  PRIMARY KEY (group_asn_id),
+  FOREIGN KEY (group_id) REFERENCES Student_groups(group_id)
+);
+
+CREATE TABLE Group_project
+(
+  group_proj_id INT NOT NULL AUTO_INCREMENT,
+  details VARCHAR(255) NOT NULL,
+  group_id INT NOT NULL,
+  PRIMARY KEY (group_proj_id),
+  FOREIGN KEY (group_id) REFERENCES Student_groups(group_id)
 );
 
 CREATE TABLE Announcement
 (
-  announcement_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  section_id SMALLINT UNSIGNED NOT NULL,
-  posted_by_user_id MEDIUMINT UNSIGNED NOT NULL,
-  posted_on DATE NOT NULL,
+  announcement_id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
   content VARCHAR(1024) NOT NULL,
+  posted_by_uid INT NOT NULL,
+  posted_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+  course_id INT NOT NULL,
   PRIMARY KEY (announcement_id),
-  FOREIGN KEY (section_id) REFERENCES Course_Section(section_id)
+  FOREIGN KEY (course_id) REFERENCES Course(course_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Discussion
 (
-  discussion_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  group_id MEDIUMINT UNSIGNED NOT NULL,
-  title VARCHAR(100) NOT NULL,
-  discussion_text VARCHAR(1024) NOT NULL,
-  posted_on DATE NOT NULL,
-  posted_by_user_id MEDIUMINT UNSIGNED NOT NULL,
+  discussion_id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  content VARCHAR(1024) NOT NULL,
+  posted_by_uid INT NOT NULL,
+  posted_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+  group_id INT NOT NULL,
   PRIMARY KEY (discussion_id),
-  FOREIGN KEY (group_id) REFERENCES Student_Groups(group_id)
+  FOREIGN KEY (group_id) REFERENCES Student_groups(group_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Comment
 (
-  comment_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  discussion_id MEDIUMINT UNSIGNED NOT NULL,
-  comment_text VARCHAR(1024) NOT NULL,
-  posted_by_user_id MEDIUMINT UNSIGNED NOT NULL,
-  posted_on DATE NOT NULL,
+  comment_id INT NOT NULL AUTO_INCREMENT,
+  content VARCHAR(1024) NOT NULL,
+  posted_by_uid INT NOT NULL,
+  posted_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+  discussion_id INT NOT NULL,
   PRIMARY KEY (comment_id),
-  FOREIGN KEY (discussion_id) REFERENCES Discussion(discussion_id)
+  FOREIGN KEY (discussion_id) REFERENCES Discussion(discussion_id) ON DELETE CASCADE
 );
 
 CREATE TABLE Files
 (
-  files_id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  discussion_id MEDIUMINT UNSIGNED NOT NULL,
-  uploaded_by_user_id MEDIUMINT UNSIGNED NOT NULL,
-  file_location VARCHAR(256) NOT NULL,
-  uploaded_on DATE NOT NULL,
-  file_type INT NOT NULL,
-  permission INT NOT NULL,
-  PRIMARY KEY (files_id),
-  FOREIGN KEY (discussion_id) REFERENCES Discussion(discussion_id)
+  file_id INT NOT NULL AUTO_INCREMENT,
+  file_name VARCHAR(30) NOT NULL,
+  file_type VARCHAR(30) NOT NULL,
+  file_location VARCHAR(255) NOT NULL,
+  uploaded_by_uid INT NOT NULL,
+  uploaded_on DATETIME DEFAULT CURRENT_TIMESTAMP,
+  discussion_id INT NOT NULL,
+  PRIMARY KEY (file_id),
+  FOREIGN KEY (discussion_id) REFERENCES Discussion(discussion_id) ON DELETE CASCADE
 );
-
-
-
