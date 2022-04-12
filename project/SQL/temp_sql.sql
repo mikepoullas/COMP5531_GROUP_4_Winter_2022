42b0b086db814f4a65deaf768779a31679e236d2
shafiq.imtiaz <shafiqimtiaz@gmail.com>
2022-04-12 15:10:32 -0400

    change

--- project/SQL/temp_sql.sql
+++ project/SQL/temp_sql.sql
@@ -147,3 +147,33 @@ FROM forum as f
 WHERE us.user_id = 10007
 ORDER BY f.forum_id
 LIMIT 10;
+
+SELECT * FROM member_of_group as mg
+JOIN student_group as g ON g.group_id = mg.group_id
+JOIN group_of_course as gc ON gc.group_id = g.group_id
+JOIN course as c ON c.course_id = gc.course_id
+JOIN student as st ON st.student_id = mg.student_id
+JOIN users as u ON u.user_id = st.user_id
+WHERE u.user_id = 10007;
+
+SELECT g.*, c.*, st.*, u.* FROM student_group as g
+    JOIN member_of_group as mg ON mg.group_id = g.group_id
+    JOIN student as st ON st.student_id = mg.student_id
+    JOIN users as u ON u.user_id = st.user_id
+    JOIN group_of_course as gc ON gc.group_id = g.group_id
+    JOIN course as c ON c.course_id = gc.course_id
+    JOIN users as us ON us.user_id = '$user_id'
+    ORDER BY g.group_id ASC;
+    
+    
+SELECT t.*, c.*, f.*, s.*, u.*, g.* FROM task as t
+    JOIN course as c ON c.course_id = t.course_id
+    JOIN group_of_course as gc ON gc.course_id = c.course_id
+    JOIN student_group as g ON g.group_id = gc.group_id
+	JOIN user_course_section as ucs ON ucs.course_id = c.course_id
+	JOIN users as us ON us.user_id = ucs.user_id
+    LEFT JOIN solution as s ON s.task_id = t.task_id
+	LEFT JOIN files as f ON f.file_id = s.file_id
+    LEFT JOIN users as u ON u.user_id = f.uploaded_by_uid
+    WHERE us.user_id = 10002 AND c.course_id = 50001
+    ORDER BY t.task_id ASC;
