9346499b0ffc1c8af0259d9d5397187cc8302cda
shafiq.imtiaz <shafiqimtiaz@gmail.com>
2022-04-25 11:43:53 -0400

    removed comments

--- project/group/group-discussion.php
+++ project/group/group-discussion.php
@@ -1,12 +1,3 @@
-<!--
-CODE CONTRIBUTOR:
-
-# COMP 5531 - GROUP 4 (Winter 2022)
-Student_ID  First_Name  Last_Name   Email
-40159305    shafiq      IMTIAZ      s_mtiaz@encs.concordia.ca
-21917730    michael     POULLAS     m_poull@encs.concordia.ca
--->
-
 <script>
     function validateDiscussion() {
 
